b059866e359ae1002e998c5e424f66c11669c5b1
v i p e r <91398017+vipertronnx@users.noreply.github.com>
2026-02-04 15:56:10 -0500

    missed the github name in the footer

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
   <footer>
     <span class="text">Copyright &copy; <?php echo date("Y"); ?></span>
     <span class="spacer">&nbsp;&nbsp;//&nbsp;&nbsp;</span>
-    <span class="text"><a target="_blank" href="https://github.com/doctor-viper/viperverse.tv">viperverse.tv</a></span>
+    <span class="text"><a target="_blank" href="https://github.com/vipertronnx/viperverse.tv">viperverse.tv</a></span>
     <span class="spacer">&nbsp;&nbsp;//&nbsp;&nbsp;</span>
     <span class="text">all rights reserved</span>
   </footer>
